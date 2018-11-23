f674e2695e9e278523abdc633becbffbacbe891e
nkazakova <natalia.kazakova@innoway.ru>
2018-11-23 14:22:32 +0300

    Refactoring to MVP

---
+++ app/src/main/java/com/sqisland/tutorial/recipes/ui/recipe/RecipePresenter.java
@@ -0,0 +1,35 @@
+package com.sqisland.tutorial.recipes.ui.recipe;
+
+import com.sqisland.tutorial.recipes.data.local.Favorites;
+import com.sqisland.tutorial.recipes.data.local.RecipeStore;
+import com.sqisland.tutorial.recipes.data.model.Recipe;
+
+public class RecipePresenter implements RecipeContract.Listener{
+    private final RecipeStore store;
+    private RecipeContract.View view;
+    private Favorites favorites;
+    private Recipe recipe;
+    
+    public RecipePresenter(RecipeStore store, RecipeContract.View view, Favorites favorites) {
+        this.store = store;
+        this.view = view;
+        this.favorites = favorites;
+    }
+    
+    public void loadRecipe(String id) {
+        recipe = store.getRecipe(id);
+        
+        if (recipe == null) {
+            view.showRecipeNotFoundError();
+        } else {
+            view.setTitle(recipe.title);
+            view.setDescription(recipe.description);
+            view.setFavorite(favorites.get(recipe.id));
+        }
+    }
+    
+    public void toggleFavorite() {
+        boolean result = favorites.toggle(recipe.id);
+        view.setFavorite(result);
+    }
+}
